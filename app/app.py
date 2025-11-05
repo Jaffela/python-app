987c93c4dca5193a7f932eb80d1f5c6d1645f0dc
jaffela <jaffela2011@gmail.com>
2025-11-05 02:29:30 +0100

    Test CI 2

--- app/app.py
+++ app/app.py
@@ -26,7 +26,11 @@ def mark_done(task_id):
             return jsonify(task)
     return jsonify({"error": "Task not found"}), 404
 
-
+@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    global tasks
+    tasks = [task for task in tasks if task["id"] != task_id]
+    return jsonify({"message": "Deleted"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
